48c9a19cd918945a3be2e8d862aae2877999f2cc
jaeung <dlwodnd00@gmail.com>
2024-06-01 03:05:07 +0900

    add tooltip and reduce loading time

--- components/map.js
+++ components/map.js
@@ -4,74 +4,54 @@ class Map {
         this.width = 648;
         this.height = 800;
         this.initialThreshold = 0.91;
-        this.map = L.map('map-view').setView([37.551, 127.043], 14); // Center on Seongdong-gu, Seoul
-        this.state = 'Paths'
+        this.map = L.map('map-view').setView([37.553, 127.043], 14); // Center on Seongdong-gu, Seoul
+        this.state = 'Paths';
 
         L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         }).addTo(this.map);
-
-        this.pathCounts = this.calculatePathCounts();
-        this.returnCounts = this.calculateReturnCounts();
-        this.rentCounts = this.calculateRentCounts();
-        this.combinedCounts = this.calculateCombinedCounts();
-        this.top10PercentThreshold = this.initialThreshold; // Initial threshold
         this.map.on('zoomend', () => this.updatePaths(this.state));
-        this.addButtons();
-    }
-    
-    initialize() {
-        this.updatePaths(this.state); // Initial rendering
     }
 
-    calculatePathCounts() {
+    initialize() { 
+        this.addButtons();
+        this.drawLegend();
+        this.initializeData();
+        this.updatePaths(this.state); 
+    }
+
+    initializeData() {
         const pathCounts = {};
+        const returnCounts = {};
+        const rentCounts = {};
+        const combinedCounts = {};
+        const markerData = {};
+
         this.data.forEach(d => {
             const start = `${d.RENT_LAT},${d.RENT_LON}`;
             const end = `${d.RTN_LAT},${d.RTN_LON}`;
             const pathKey = [start, end].sort().join("_");
 
+            // Path counts
             if (!pathCounts[pathKey]) {
                 pathCounts[pathKey] = 0;
             }
             pathCounts[pathKey]++;
-        });
-        return pathCounts;
-    }
 
-    calculateReturnCounts() {
-        const returnCounts = {};
-        this.data.forEach(d => {
-            const end = `${d.RTN_LAT},${d.RTN_LON}`;
-
+            // Return counts
             if (!returnCounts[end]) {
                 returnCounts[end] = 0;
             }
             returnCounts[end]++;
-        });
-        return returnCounts;
-    }
 
-    calculateRentCounts() {
-        const rentCounts = {};
-        this.data.forEach(d => {
-            const start = `${d.RENT_LAT},${d.RENT_LON}`;
-
+            // Rent counts
             if (!rentCounts[start]) {
                 rentCounts[start] = 0;
             }
             rentCounts[start]++;
-        });
-        return rentCounts;
-    }
 
-    calculateCombinedCounts() {
-        const combinedCounts = {};
-        this.data.forEach(d => {
-            const start = `${d.RENT_LAT},${d.RENT_LON}`;
-            const end = `${d.RTN_LAT},${d.RTN_LON}`;
-
+            // Combined counts
             if (!combinedCounts[start]) {
                 combinedCounts[start] = 0;
             }
@@ -80,98 +60,27 @@ class Map {
             }
             combinedCounts[start]++;
             combinedCounts[end]++;
-        });
-        return combinedCounts;
-    }
 
-    updatePaths(state) {
-        this.state = state
-        if(this.state !== 'Paths') 
-            return;
-        // Adjust threshold based on zoom level
-        const zoomLevel = this.map.getZoom();
-        this.top10PercentThreshold = Math.min(1, Math.max(0, this.initialThreshold - (zoomLevel - 14) * 0.05));
-        const counts = Object.values(this.pathCounts).sort((a, b) => a - b);
-        const thresholdValue = d3.quantile(counts, this.top10PercentThreshold);
-
-        this.topPaths = Object.entries(this.pathCounts)
-            .filter(([key, count]) => count >= thresholdValue)
-            .map(([key, count]) => ({ key, count }));
-
-        this.topPaths.sort((a, b) => a.count - b.count);
-
-        this.colorScale = d3.scaleQuantize()
-            .domain([0, this.topPaths.length])
-            .range(d3.range(10).map(i => d3.interpolateReds(i / 9)));
-
-        this.thicknessScale = d3.scaleQuantize()
-            .domain([0, this.topPaths.length])
-            .range(d3.range(10).map(i => 2.0 + i / 2));
-
-        this.clearPaths();
-        this.drawPaths();
-        this.drawLegend();
-    }
-
-    clearPaths() {
-        this.map.eachLayer((layer) => {
-            if (layer instanceof L.Polyline || layer instanceof L.CircleMarker) {
-                this.map.removeLayer(layer);
+            // Marker data
+            if (!markerData[start]) {
+                markerData[start] = { name: d.RENT_NM, rentCount: 0, returnCount: 0 };
             }
+            if (!markerData[end]) {
+                markerData[end] = { name: d.RTN_NM, rentCount: 0, returnCount: 0 };
+            }
+            markerData[start].rentCount++;
+            markerData[end].returnCount++;
         });
-    }
-
-    drawPaths() {
-        this.topPaths.forEach((path, index) => {
-            const { key, count } = path;
-            const [start, end] = key.split("_").map(d => d.split(",").map(Number));
-            const weight = this.thicknessScale(index);
-            const color = this.colorScale(index);
-
-            L.polyline([start, end], {
-                color: color,
-                weight: weight,
-                opacity: 0.7
-            }).addTo(this.map);
-        });
-
-        Object.keys(this.pathCounts).forEach(pathKey => {
-            const [start, end] = pathKey.split("_").map(d => d.split(",").map(Number));
-
-            L.circleMarker(start, {
-                color: 'red',
-                radius: 3,
-                opacity: 1,
-                weight: 1, // 테두리 두께
-                fillOpacity: 0.2 // 중간의 투명도
-            }).addTo(this.map);
-
-            L.circleMarker(end, {
-                color: 'red',
-                radius: 3,
-                opacity: 1,
-                weight: 1, // 테두리 두께
-                fillOpacity: 0.2 // 중간의 투명도
-            }).addTo(this.map);
-        });
+        this.pathCounts = pathCounts;
+        this.returnCounts = returnCounts;
+        this.rentCounts = rentCounts;
+        this.combinedCounts = combinedCounts;
+        this.markerData = markerData;
     }
 
     drawLegend() {
         const legendContainer = d3.select('#legend');
-        legendContainer.html(""); // Clear previous content
-
-        // Set styles directly in JavaScript
-        legendContainer
-            .style('position', 'absolute')
-            .style('left', '47.4%')
-            .style('bottom', '20px')
-            .style('width', '50%')
-            .style('height', '55px')
-            .style('background-color', 'white')
-            .style('padding', '10px 0px 0px 0px')
-            .style('border-radius', '5px')
-            .style('box-shadow', '0 0 10px rgba(0, 0, 0, 0.5)')
-            .style('z-index', '1000');
+        legendContainer.html("");
 
         const svg = legendContainer.append('svg')
             .attr('width', '100%')
@@ -196,7 +105,6 @@ class Map {
             .attr('height', '20')
             .attr('fill', 'url(#gradient)');
 
-        // Add description text above the colormap
         svg.append('text')
             .attr('x', '50%')
             .attr('y', '15')
@@ -205,10 +113,9 @@ class Map {
             .style('font-weight', 'bold')
             .text('Traffic Top N% of Paths');
 
-        // Add 0%, 50%, 100% labels below the colormap
         const labels = [0, 50, 100];
         const labelGroup = svg.append('g')
-            .attr('transform', 'translate(0, 20)'); // Adjust label position
+            .attr('transform', 'translate(0, 20)');
 
         labels.forEach((d, i) => {
             labelGroup.append('text')
@@ -220,22 +127,23 @@ class Map {
         });
     }
 
+    hideLegend() {
+        d3.select('#legend').style('display', 'none');
+    }
+
+    showLegend() {
+        d3.select('#legend').style('display', 'block');
+    }
+
     addButtons() {
         const buttonContainer = d3.select('#map-btn')
             .attr('class', 'button-container')
-            .style('position', 'absolute')
-            .style('left', '20px')
-            .style('bottom', '20px')
-            .style('background-color', 'white')
-            .style('padding', '10px')
-            .style('border-radius', '5px')
-            .style('box-shadow', '0 0 10px rgba(0, 0, 0, 0.5)')
-            .style('z-index', '1000');
+            .style('display', 'block');
 
         const buttons = [
-            { id: 'return-locations', text: 'Return', handler: () => this.showReturnLocations() },
-            { id: 'rent-locations', text: 'Rent', handler: () => this.showRentLocations() },
-            { id: 'combined-locations', text: 'Combined', handler: () => this.showCombinedLocations() },
+            { id: 'return-locations', text: 'Return Count', handler: () => this.showReturnLocations() },
+            { id: 'rent-locations', text: 'Rental Count', handler: () => this.showRentLocations() },
+            { id: 'combined-locations', text: 'Total', handler: () => this.showCombinedLocations() },
             { id: 'Paths', text: 'Paths', handler: () => this.updatePaths('Paths') }
         ];
 
@@ -243,63 +151,132 @@ class Map {
             buttonContainer.append('button')
                 .attr('id', button.id)
                 .text(button.text)
-                .on('click', button.handler)
-                .style('margin', '3px 4px 3px 4px')
-                .style('padding', '4px 6px 4px 6px')
-                .style('border', 'none')
-                .style('border-radius', '3px')
-                .style('background-color', '#777777')
-                .style('color', 'white')
-                .style('font-size', '14px')
-                .style('cursor', 'pointer')
-                .on('mouseover', function() {
-                    d3.select(this).style('background-color', '#111111');
-                })
-                .on('mouseout', function() {
-                    d3.select(this).style('background-color', '#666666');
-                });
+                .on('click', button.handler);
+        });
+    }
+
+    updatePaths(state) {
+        this.state = state;
+        if (this.state !== 'Paths') {
+            this.hideLegend();
+            return;
+        }
+        this.showLegend();
+
+        // Adjust threshold based on zoom level
+        const zoomLevel = this.map.getZoom();
+        this.top10PercentThreshold = Math.min(1, Math.max(0, this.initialThreshold - (zoomLevel - 14) * 0.06));
+        const counts = Object.values(this.pathCounts).sort((a, b) => a - b);
+        const thresholdValue = d3.quantile(counts, this.top10PercentThreshold);
+
+        this.topPaths = Object.entries(this.pathCounts)
+            .filter(([key, count]) => count >= thresholdValue)
+            .map(([key, count]) => ({ key, count }));
+
+        this.topPaths.sort((a, b) => a.count - b.count);
+
+        this.colorScale = d3.scaleQuantize()
+            .domain([0, this.topPaths.length])
+            .range(d3.range(10).map(i => d3.interpolateReds(i / 9)));
+
+        this.thicknessScale = d3.scaleQuantize()
+            .domain([0, this.topPaths.length])
+            .range(d3.range(10).map(i => 1.5 + i * 0.8 ));
+
+        this.clearPaths();
+        this.drawPaths();
+    }
+
+    clearPaths() {
+        this.map.eachLayer((layer) => {
+            if (layer instanceof L.Polyline || layer instanceof L.CircleMarker) {
+                this.map.removeLayer(layer);
+            }
+        });
+    }
+
+    drawPaths() {
+        this.topPaths.forEach((path, index) => {
+            const { key, count } = path;
+            const [start, end] = key.split('_').map(d => d.split(',').map(Number));
+            const weight = this.thicknessScale(index);
+            const color = this.colorScale(index);
+
+            const from = start.join(',');
+            const to = end.join(',');
+
+            L.polyline([start, end], {
+                color: color,
+                weight: weight,
+                opacity: 0.7
+            }).bindTooltip(`<strong>${this.markerData[from].name} <-> ${this.markerData[to].name}</strong><br>
+                            Path Count: ${count}`).addTo(this.map);
+        });
+
+        Object.keys(this.markerData).forEach(locationKey => {
+            const marker = this.markerData[locationKey];
+            const [lat, lon] = locationKey.split(',').map(Number);
+    
+            L.circleMarker([lat, lon], {
+                color: 'red',
+                radius: 4,
+                opacity: 1,
+                weight: 1, 
+                fillOpacity: 1 
+            }).bindTooltip(`<strong>${marker.name}</strong><br>
+                            Rental Count: ${marker.rentCount}<br>
+                            Return Count: ${marker.returnCount}<br>
+                            Total: ${marker.rentCount + marker.returnCount}<br>`).addTo(this.map);
         });
     }
 
     showReturnLocations() {
         this.clearPaths();
-        this.state = 'Bubble'
+        this.hideLegend();
+        this.state = 'Bubble';
         Object.entries(this.returnCounts).forEach(([location, count]) => {
             const [lat, lon] = location.split(',').map(Number);
             L.circleMarker([lat, lon], {
                 color: 'blue',
-                radius: 1 + Math.sqrt(count / 2),
+                radius: Math.sqrt(count * 0.8),
                 fillOpacity: 0.5,
                 weight: 2
-            }).addTo(this.map);
+            }).bindTooltip(`<strong>${this.markerData[location].name}</strong><br>
+                            Return Count: ${this.markerData[location].returnCount}`).addTo(this.map);
         });
     }
 
     showRentLocations() {
         this.clearPaths();
-        this.state = 'Bubble'
+        this.hideLegend();
+        this.state = 'Bubble';
         Object.entries(this.rentCounts).forEach(([location, count]) => {
             const [lat, lon] = location.split(',').map(Number);
             L.circleMarker([lat, lon], {
                 color: 'green',
-                radius: 1 + Math.sqrt(count / 2),
+                radius: Math.sqrt(count * 0.8),
                 fillOpacity: 0.5,
                 weight: 2
-            }).addTo(this.map);
+            }).bindTooltip(`<strong>${this.markerData[location].name}</strong><br>
+                            Rental Count: ${this.markerData[location].rentCount}`).addTo(this.map);
         });
     }
 
     showCombinedLocations() {
         this.clearPaths();
-        this.state = 'Bubble'
+        this.hideLegend();
+        this.state = 'Bubble';
         Object.entries(this.combinedCounts).forEach(([location, count]) => {
             const [lat, lon] = location.split(',').map(Number);
             L.circleMarker([lat, lon], {
                 color: 'purple',
-                radius: 1 + Math.sqrt(count / 2),
+                radius: Math.sqrt(count * 0.8),
                 fillOpacity: 0.5,
                 weight: 2
-            }).addTo(this.map);
+            }).bindTooltip(`<strong>${this.markerData[location].name}</strong><br>
+                            Rental Count: ${this.markerData[location].rentCount}<br>
+                            Return Count: ${this.markerData[location].returnCount}<br>
+                            Total: ${this.markerData[location].rentCount + this.markerData[location].returnCount}<br>`).addTo(this.map);
         });
     }
 }
