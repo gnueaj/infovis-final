4907f9810c091cdd57d7d069b159843a70bd1c16
jaeung <dlwodnd00@gmail.com>
2024-05-31 17:51:22 +0900

    add rent and return count visualization

--- components/map.js
+++ components/map.js
@@ -3,8 +3,9 @@ class Map {
         this.data = data;
         this.width = 648;
         this.height = 800;
-        this.initialThreshold = 0.9;
-        this.map = L.map('map-view').setView([37.551, 127.0406], 14); // Center on Seongdong-gu, Seoul
+        this.initialThreshold = 0.91;
+        this.map = L.map('map-view').setView([37.551, 127.043], 14); // Center on Seongdong-gu, Seoul
+        this.state = 'Paths'
 
         L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
             maxZoom: 19,
@@ -12,8 +13,16 @@ class Map {
         }).addTo(this.map);
 
         this.pathCounts = this.calculatePathCounts();
+        this.returnCounts = this.calculateReturnCounts();
+        this.rentCounts = this.calculateRentCounts();
+        this.combinedCounts = this.calculateCombinedCounts();
         this.top10PercentThreshold = this.initialThreshold; // Initial threshold
         this.map.on('zoomend', () => this.updatePaths());
+        this.addButtons();
+    }
+    
+    initialize() {
+        this.updatePaths(); // Initial rendering
     }
 
     calculatePathCounts() {
@@ -31,11 +40,56 @@ class Map {
         return pathCounts;
     }
 
+    calculateReturnCounts() {
+        const returnCounts = {};
+        this.data.forEach(d => {
+            const end = `${d.RTN_LAT},${d.RTN_LON}`;
+
+            if (!returnCounts[end]) {
+                returnCounts[end] = 0;
+            }
+            returnCounts[end]++;
+        });
+        return returnCounts;
+    }
+
+    calculateRentCounts() {
+        const rentCounts = {};
+        this.data.forEach(d => {
+            const start = `${d.RENT_LAT},${d.RENT_LON}`;
+
+            if (!rentCounts[start]) {
+                rentCounts[start] = 0;
+            }
+            rentCounts[start]++;
+        });
+        return rentCounts;
+    }
+
+    calculateCombinedCounts() {
+        const combinedCounts = {};
+        this.data.forEach(d => {
+            const start = `${d.RENT_LAT},${d.RENT_LON}`;
+            const end = `${d.RTN_LAT},${d.RTN_LON}`;
+
+            if (!combinedCounts[start]) {
+                combinedCounts[start] = 0;
+            }
+            if (!combinedCounts[end]) {
+                combinedCounts[end] = 0;
+            }
+            combinedCounts[start]++;
+            combinedCounts[end]++;
+        });
+        return combinedCounts;
+    }
+
     updatePaths() {
+        if(this.state !== 'Paths') 
+            return;
         // Adjust threshold based on zoom level
         const zoomLevel = this.map.getZoom();
-        this.top10PercentThreshold = Math.min(1, Math.max(0, this.initialThreshold - (zoomLevel - 14) * 0.06));
-
+        this.top10PercentThreshold = Math.min(1, Math.max(0, this.initialThreshold - (zoomLevel - 14) * 0.05));
         const counts = Object.values(this.pathCounts).sort((a, b) => a - b);
         const thresholdValue = d3.quantile(counts, this.top10PercentThreshold);
 
@@ -87,14 +141,16 @@ class Map {
                 color: 'red',
                 radius: 3,
                 opacity: 1,
-                fillOpacity: 1
+                weight: 1, // 테두리 두께
+                fillOpacity: 0.2 // 중간의 투명도
             }).addTo(this.map);
 
             L.circleMarker(end, {
                 color: 'red',
                 radius: 3,
                 opacity: 1,
-                fillOpacity: 1
+                weight: 1, // 테두리 두께
+                fillOpacity: 0.2 // 중간의 투명도
             }).addTo(this.map);
         });
     }
@@ -106,7 +162,7 @@ class Map {
         // Set styles directly in JavaScript
         legendContainer
             .style('position', 'absolute')
-            .style('left', '47%')
+            .style('left', '47.4%')
             .style('bottom', '20px')
             .style('width', '50%')
             .style('height', '55px')
@@ -119,7 +175,7 @@ class Map {
         const svg = legendContainer.append('svg')
             .attr('width', '100%')
             .attr('height', '100%')
-			.attr('viewBox', '0 0 270 43')
+            .attr('viewBox', '0 0 270 43');
 
         const gradient = svg.append('defs').append('linearGradient')
             .attr('id', 'gradient')
@@ -145,6 +201,7 @@ class Map {
             .attr('y', '15')
             .attr('text-anchor', 'middle')
             .style('font-size', '12px')
+            .style('font-weight', 'bold')
             .text('Traffic Top N% of Paths');
 
         // Add 0%, 50%, 100% labels below the colormap
@@ -162,7 +219,86 @@ class Map {
         });
     }
 
-    initialize() {
-        this.updatePaths(); // Initial rendering
+    addButtons() {
+        const buttonContainer = d3.select('#map-btn')
+            .attr('class', 'button-container')
+            .style('position', 'absolute')
+            .style('left', '20px')
+            .style('bottom', '20px')
+            .style('background-color', 'white')
+            .style('padding', '10px')
+            .style('border-radius', '5px')
+            .style('box-shadow', '0 0 10px rgba(0, 0, 0, 0.5)')
+            .style('z-index', '1000');
+
+        const buttons = [
+            { id: 'return-locations', text: 'Return', handler: () => this.showReturnLocations() },
+            { id: 'rent-locations', text: 'Rent', handler: () => this.showRentLocations() },
+            { id: 'combined-locations', text: 'Combined', handler: () => this.showCombinedLocations() },
+            { id: 'Paths', text: 'Paths', handler: () => this.updatePaths() }
+        ];
+
+        buttons.forEach(button => {
+            buttonContainer.append('button')
+                .attr('id', button.id)
+                .text(button.text)
+                .on('click', button.handler)
+                .style('margin', '3px 4px 3px 4px')
+                .style('padding', '4px 6px 4px 6px')
+                .style('border', 'none')
+                .style('border-radius', '3px')
+                .style('background-color', '#777777')
+                .style('color', 'white')
+                .style('font-size', '14px')
+                .style('cursor', 'pointer')
+                .on('mouseover', function() {
+                    d3.select(this).style('background-color', '#111111');
+                })
+                .on('mouseout', function() {
+                    d3.select(this).style('background-color', '#666666');
+                });
+        });
+    }
+
+    showReturnLocations() {
+        this.clearPaths();
+        this.state = 'Return'
+        Object.entries(this.returnCounts).forEach(([location, count]) => {
+            const [lat, lon] = location.split(',').map(Number);
+            L.circleMarker([lat, lon], {
+                color: 'blue',
+                radius: 1 + Math.sqrt(count / 2),
+                fillOpacity: 0.5,
+                weight: 2
+            }).addTo(this.map);
+        });
+    }
+
+    showRentLocations() {
+        this.clearPaths();
+        this.state = 'Rent'
+        Object.entries(this.rentCounts).forEach(([location, count]) => {
+            const [lat, lon] = location.split(',').map(Number);
+            L.circleMarker([lat, lon], {
+                color: 'green',
+                radius: 1 + Math.sqrt(count / 2),
+                fillOpacity: 0.5,
+                weight: 2
+            }).addTo(this.map);
+        });
+    }
+
+    showCombinedLocations() {
+        this.clearPaths();
+        this.state = 'Combined'
+        Object.entries(this.combinedCounts).forEach(([location, count]) => {
+            const [lat, lon] = location.split(',').map(Number);
+            L.circleMarker([lat, lon], {
+                color: 'purple',
+                radius: 1 + Math.sqrt(count / 2),
+                fillOpacity: 0.5,
+                weight: 2
+            }).addTo(this.map);
+        });
     }
 }
